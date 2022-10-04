61b21b8f111197c407f39f6543c63227bc82577a
Ben Jeurissen <ben.jeurissen@gmail.com>
2022-10-04 22:48:59 +0200

    Update adc_crlb_demo.m

--- adc_crlb_demo.m
+++ adc_crlb_demo.m
@@ -1,12 +1,12 @@
 function adc_crlb_demo()
-    % Calculates the CRLB for ADC (second diagonal element of inverse of Fisher information matrix), as a function of b and sigma
+    % Calculates the CRLB for ADC as a function dependent variables in x, and independent variables b and sigma
     function val = cost(x,b,sigma)
         if size(unique(b),1) > 1
             df = @(x,b) [exp(-b*x(2)) -b.*x(1).*exp(-b*x(2))]; % partial derivates of ADC model with respect to S_0 (x(1)) and ADC (x(2))
             dy = df(x,b);
-            I = (dy'*dy)./sigma^2;
-            invI = inv(I+eps);
-            val = invI(2,2);
+            I = (dy'*dy)./sigma^2; % Fisher information matrix (FIM)
+            invI = inv(I+eps); % Inverse of the FIM
+            val = invI(2,2); % Second diagonal element corresponds to the lower bound on the variance of ADC (x(2)) 
         else
             val = Inf;
         end
