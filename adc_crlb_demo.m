4a4f5b0c0c99b6f3cf6e308a31763b76b3346edb
Ben Jeurissen <ben.jeurissen@gmail.com>
2023-08-23 11:53:14 +0200

    Add support for lowest b-value > 0

--- adc_crlb_demo.m
+++ adc_crlb_demo.m
@@ -15,20 +15,22 @@ function adc_crlb_demo()
 options=optimoptions('fmincon','OptimalityTolerance',1e-12,'StepTolerance',1e-20,'Display','none');%,'MaxFunctionEvaluations',inf,'MaxIterations',3000);
 sigma=1; % can be set to any value, as it does not affect the optimum
 
+low_b = 0.0; % mininally achievable b-value
+
 % What is the optimal b-value for a given ADC?
 adcs = 0.5:0.5:2;
-bmaxes = 0.01:0.01:3;
+bmaxes = low_b+0.01:0.01:3;
 figure; hold all;
 for i = 1:size(adcs,2)
     c = zeros(size(bmaxes));
     for j = 1:size(bmaxes,2)
-        c(j) = cost([1;adcs(i)],[0; bmaxes(j)],sigma);
+        c(j) = cost([1;adcs(i)],[low_b; bmaxes(j)],sigma);
     end
-    [b_opt, val_opt] = fmincon(@(b)cost([1;adcs(i)],[0; b],sigma),1/adcs(i),[],[],[],[],0,[],[],options);
+    [b_opt, val_opt] = fmincon(@(b)cost([1;adcs(i)],[low_b; b],sigma),low_b+1.1/adcs(i),[],[],[],[],low_b,[],[],options);
     h = plot(bmaxes,sqrt(val_opt)./sqrt(c),'LineWidth',2); line_color = get(h,'Color');
     h = scatter(max(b_opt),1,50,line_color,'filled'); set(h,'HandleVisibility','off');
 end
-xlabel(['b (ms/μm^2)']); ylabel('SNR relative to optimum');
+xlabel('b (ms/μm^2)'); ylabel('SNR relative to optimum');
 legend(arrayfun(@(x) ['ADC = ' num2str(x) ' μm^2/ms'],adcs,'UniformOutput',false))
 
 
@@ -36,16 +38,16 @@ legend(arrayfun(@(x) ['ADC = ' num2str(x) ' μm^2/ms'],adcs,'UniformOutput',fals
 adcs = 0.1:0.01:1.5;
 b_opts = zeros(size(adcs));
 for i = 1:size(adcs,2)
-    b_opts(i) = fmincon(@(b)cost([1;adcs(i)],[0; b],sigma),1/adcs(i),[],[],[],[],0,[],[],options);
+    b_opts(i) = fmincon(@(b)cost([1;adcs(i)],[low_b; b],sigma),low_b+1.1/adcs(i),[],[],[],[],low_b,[],[],options);
 end
 figure;
 plot(adcs,b_opts,'LineWidth',2); hold all;
-plot(adcs,1./adcs,'LineWidth',2);
+plot(adcs,low_b+1.1./adcs,'LineWidth',2);
 xlim([0 max(adcs)]); xlabel('ADC'); ylabel('b_{opt}');
-legend('optimal','1/ADC heuristic')
+legend('optimal','low\_b+1.1/ADC heuristic')
 
 
-% What is the optimal proportion of b=0 samples?
+% What is the optimal proportion of low b-value samples?
 adc = 1;
 bmax = 1/adc;
 figure; hold all;
@@ -57,13 +59,13 @@ for i = 1:size(ns,2)
     c = zeros(size(nb0s));
     for j = 1:size(nb0s,2)
         nb0 = nb0s(j);
-        c(j) = cost([1;adc],[zeros([nb0 1]); ones([n-nb0 1])*bmax],sigma);
+        c(j) = cost([1;adc],[ones([nb0 1])*low_b; ones([n-nb0 1])*bmax],sigma);
     end
     h = plot(nb0s./n,sqrt(c(1))./sqrt(c),'LineWidth',2); line_color = get(h,'Color');
     [~,idx] = min(c);
     h = scatter(nb0s(idx)./n,sqrt(c(1))./sqrt(c(idx)),50,line_color,'filled'); set(h,'HandleVisibility','off');
 end
-xlabel('proportion of NDWI scans'); ylabel('SNR gain relative to using 1 NDWI scan');
+xlabel(['proportion of b = ' num2str(low_b) ' ms/μm^2 scans']); ylabel(['SNR gain relative to using 1 b = ' num2str(low_b) ' ms/μm^2 scan']);
 legend(arrayfun(@(x) ['n = ' num2str(x)],ns,'UniformOutput',false))
 
 end
